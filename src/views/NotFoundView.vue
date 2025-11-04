7b30bedb2bf1aa52e5535fc93b9ac9b0178a36a0
Vinicius Araujo <dev.viniciusbarboza@gmail.com>
2025-11-04 16:00:56 -0300

    Adicionado pagina de NotFound

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="not-found">
+    <h1>Página não encontrada</h1>
+    <p>A página que você está procurando não existe.</p>
+  </div>
+</template>
+
+<style>
+.not-found {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 60vh;
+  text-align: center;
+}
+
+h1 {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+}
+
+p {
+  font-size: 1.2rem;
+}
+</style>
